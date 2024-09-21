a0d3a4e25e393c895625e7aa16fd3913abd6176f
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2024-09-21 14:46:04 +0100

    Update 0-lockboxes.py
    make change

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" Module that checks locked boxes """
+"""This script unlocks list of lists"""
 
 
 def canUnlockAll(boxes):
-    '''
-    This function can determine if all the boxes can be opened
-    '''
-    Unlocked = [0]
-    for id_box in Unlocked:
-        for b in boxes[id_box]:
-            if b not in Unlocked:
-                Unlocked.append(b)
-    for i in range(len(boxes)):
-        if i not in Unlocked:
-            return False
-    return True
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
+    """
+
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
